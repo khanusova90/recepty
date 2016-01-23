4ab2d6e1f3595c843996ccb2175eabdd182f1cd4
khanusova90 <k.hanusova90@gmail.com>
2016-01-23 13:42:45 +0100

    Spring data JPA

--- src/main/java/cz/ppro/recepty/dao/impl/DaoImpl.java
+++ src/main/java/cz/ppro/recepty/dao/impl/DaoImpl.java
@@ -1,7 +1,20 @@
 package cz.ppro.recepty.dao.impl;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import cz.ppro.recepty.dao.AbstractDao;
 
-public class DaoImpl /*extends HibernateDaoSupport*/ implements AbstractDao{
+@Repository
+public class DaoImpl implements AbstractDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
 
 }
--- src/main/java/cz/ppro/recepty/domain/Recipe.java
+++ src/main/java/cz/ppro/recepty/domain/Recipe.java
@@ -4,12 +4,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+//@Entity
+//@Table(name = "Recipe")
 public class Recipe {
-	
+	//
+	// @Id
+	// @GeneratedValue(generator = "increment")
+	// @GenericGenerator(name = "increment", strategy = "increment")
+	private Long recipeId;
+
 	private List<Ingredience> ingrediences;
 	private List<Meal> meals;
 	private Date time;
-	
+
 	/*
 	 * Hodnoceni receptu
 	 */
@@ -19,57 +26,65 @@ public class Recipe {
 	 */
 	private int rateCount;
 	private String preparationProcess;
-	
+
 	private final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-	
+
 	public Recipe() {
 		this.rateCount = 0;
 	}
-	
+
+	public Long getRecipeId() {
+		return recipeId;
+	}
+
+	public void setRecipeId(Long recipeId) {
+		this.recipeId = recipeId;
+	}
+
 	public List<Ingredience> getIngrediences() {
 		return ingrediences;
 	}
-	
+
 	public void setIngrediences(List<Ingredience> ingrediences) {
 		this.ingrediences = ingrediences;
 	}
-	
+
 	public List<Meal> getMeals() {
 		return meals;
 	}
-	
+
 	public void setMeals(List<Meal> meals) {
 		this.meals = meals;
 	}
-	
+
 	public String getPreparationProcess() {
 		return preparationProcess;
 	}
-	
+
 	public void setPreparationProcess(String preparationProcess) {
 		this.preparationProcess = preparationProcess;
 	}
-	
+
 	public float getRating() {
 		return rating;
 	}
-	
+
 	public void setRating(float rating) {
 		this.rating = rating;
 	}
-	
+
 	public Date getTime() {
 		return time;
 	}
-	
+
 	public void setTime(Date time) {
 		this.time = time;
 	}
-	
+
 	public int getRateCount() {
 		return rateCount;
 	}
-	
+
 	public void setRateCount(int rateCount) {
 		this.rateCount = rateCount;
 	}
