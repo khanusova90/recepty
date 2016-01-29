fc5668d6acabc15083d546a53a22eed3d4fe35b9
khanusova90 <k.hanusova90@gmail.com>
2016-01-29 12:39:17 +0100

    Vyhledavani apod. podle Recipe misto ID

--- src/main/java/cz/ppro/recepty/repository/RecipeIngredientRepository.java
+++ src/main/java/cz/ppro/recepty/repository/RecipeIngredientRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import cz.ppro.recepty.domain.Recipe;
 import cz.ppro.recepty.domain.RecipeIngredient;
 
 @Repository
 public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
 
-	public List<RecipeIngredient> findByRecipe_IdRecipe(Long idRecipe);
+	public List<RecipeIngredient> findByRecipe(Recipe recipe);
 
 }
