6072891a68a6d161d96c00c79ce6f11b654bf956
khanusova90 <k.hanusova90@gmail.com>
2016-01-30 05:54:12 +0100

    Vyhledavani podle ingredienci

--- src/main/java/cz/ppro/recepty/controller/RecipeController.java
+++ src/main/java/cz/ppro/recepty/controller/RecipeController.java
@@ -1,12 +1,15 @@
 package cz.ppro.recepty.controller;
 
 import java.util.List;
+import java.util.Locale;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import cz.ppro.recepty.domain.AppUser;
 import cz.ppro.recepty.domain.Category;
+import cz.ppro.recepty.domain.Ingredient;
 import cz.ppro.recepty.domain.Recipe;
 import cz.ppro.recepty.domain.RecipeIngredient;
 import cz.ppro.recepty.service.IngredientService;
@@ -29,6 +33,9 @@ class RecipeController {
 
 	@Autowired
 	private IngredientService ingredientService;
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public String showRecipes(Model model) {
@@ -66,27 +73,29 @@ class RecipeController {
 		return "redirect:/listedRecipes";
 	}
 
-	@RequestMapping(value = "/doDeleteRecipe", method = RequestMethod.POST)
-	public String deleteRecipe(Model model, HttpSession session, @RequestParam("recipe") Recipe recipe) {
-		recipeService.deleteRecipe(recipe);
-		AppUser user = (AppUser) session.getAttribute("user");
-		model.addAttribute("recipes", recipeService.getAllRecipesByUser(user));
-		return "listedRecipes";
-	}
+	// @RequestMapping(value = "/doDeleteRecipe", method = RequestMethod.POST)
+	// public String deleteRecipe(Model model, HttpSession session,
+	// @RequestParam("recipe") Recipe recipe) {
+	// recipeService.deleteRecipe(recipe);
+	// AppUser user = (AppUser) session.getAttribute("user");
+	// model.addAttribute("recipes", recipeService.getAllRecipesByUser(user));
+	// return "listedRecipes";
+	// }
 
 	@RequestMapping(value = "/search")
-	public String showDishes(Model model) {
+	public String showDishes(Model model, Locale locale) {
 		model.addAttribute("ingredients", ingredientService.getAll());
 		model.addAttribute("recipes", null);
+		model.addAttribute("selectedIngredients", messageSource.getMessage("insertIngredients", null, locale));
 		return "searchByIngredients";
 	}
 
-	@RequestMapping(value = "/searchBy")
-	public String showDishes(Model model, @RequestParam("ingredients") String ingredientsString) {
-
-		// TODO: recepty podle ingredienci
-		model.addAttribute("reicpes", null);
-		return "searchByIngredients";
+	@RequestMapping(value = "/searchBy", method = RequestMethod.POST)
+	public String showDishes(Model model, @ModelAttribute("selectedIngredients") String ingredientsString) {
+		List<Ingredient> ingredients = ingredientService.splitIngredients(ingredientsString);
+		List<Recipe> recipes = recipeService.findRecipesByIngredients(ingredients);
+		model.addAttribute("recipes", recipes);
+		return "listedRecipes";
 	}
 
 	public String findRecipeByName(Model model) {
